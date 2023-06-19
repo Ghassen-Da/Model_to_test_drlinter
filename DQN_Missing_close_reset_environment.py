a4800af283bec8b838e8df77f67e884759ebe2e9
Ghassen-Da <ghassendaoud99@gmail.com>
2023-06-19 15:31:02 -0400

    config.txt

--- DQN_Missing_close_reset_environment.py
+++ DQN_Missing_close_reset_environment.py
@@ -5,7 +5,7 @@ import os
 import datetime
 from statistics import mean
 from gym import wrappers
- 
+
 
 class MyModel(tf.keras.Model):
     def __init__(self, num_states, hidden_units, num_actions):
@@ -15,7 +15,8 @@ class MyModel(tf.keras.Model):
         for i in hidden_units:
             self.hidden_layers.append(tf.keras.layers.Dense(
                 i, activation='tanh', kernel_initializer='RandomNormal'))
-        self.output_layer = tf.keras.layers.Dense(num_actions, activation='linear', kernel_initializer='RandomNormal') 
+        self.output_layer = tf.keras.layers.Dense(
+            num_actions, activation='linear', kernel_initializer='RandomNormal')
 
     @tf.function
     def call(self, inputs):
@@ -50,7 +51,7 @@ class DQN:
         states_next = np.asarray([self.experience['s2'][i] for i in ids])
         dones = np.asarray([self.experience['done'][i] for i in ids])
         value_next = np.max(TargetNet.predict(states_next), axis=1)
-        actual_values = np.where(dones, rewards, rewards+self.gamma*value_next) 
+        actual_values = np.where(dones, rewards, rewards+self.gamma*value_next)
 
         with tf.GradientTape() as tape:
             selected_action_values = tf.math.reduce_sum(
@@ -62,10 +63,10 @@ class DQN:
         return loss
 
     def get_action(self, states, epsilon):
-        if np.random.random() < epsilon: 
+        if np.random.random() < epsilon:
             return np.random.choice(self.num_actions)
         else:
-            return np.argmax(self.predict(np.atleast_2d(states))[0]) 
+            return np.argmax(self.predict(np.atleast_2d(states))[0])
 
     def add_experience(self, exp):
         if len(self.experience['s']) >= self.max_experiences:
@@ -85,19 +86,19 @@ def play_game(env, TrainNet, TargetNet, epsilon, copy_step):
     rewards = 0
     iter = 0
     done = False
-    observations = env.reset() 
+    observations = env.reset()
     losses = list()
     while not done:
         action = TrainNet.get_action(observations, epsilon)
         prev_observations = observations
-        observations, reward, done, _ = env.step(action) 
+        observations, reward, done, _ = env.step(action)
         rewards += reward
-        if done: 
-            reward = -200
-            env.reset() 
+        #if done:
+         #   reward = -200
+          #  env.reset()
 
         exp = {'s': prev_observations, 'a': action, 'r': reward, 's2': observations, 'done': done}
-        TrainNet.add_experience(exp) 
+        TrainNet.add_experience(exp)
         loss = TrainNet.train(TargetNet)
         if isinstance(loss, int):
             losses.append(loss)
@@ -105,34 +106,34 @@ def play_game(env, TrainNet, TargetNet, epsilon, copy_step):
             losses.append(loss.numpy())
         iter += 1
         if iter % copy_step == 0:
-            TargetNet.copy_weights(TrainNet) 
+            TargetNet.copy_weights(TrainNet)
     return rewards, mean(losses)
 
 
 def main():
-    env = gym.make('CartPole-v0') 
-    gamma = 0.99 
-    copy_step = 25 
+    env = gym.make('CartPole-v0')
+    gamma = 0.99
+    copy_step = 25
     num_states = len(env.observation_space.sample())
     num_actions = env.action_space.n
     hidden_units = [200, 200]
     max_experiences = 10000
     min_experiences = 100
-    batch_size = 32 
-    lr = 1e-2 
+    batch_size = 32
+    lr = 1e-2
     current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     log_dir = 'logs/dqn/' + current_time
     summary_writer = tf.summary.create_file_writer(log_dir)
 
     TrainNet = DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, lr)
     TargetNet = DQN(num_states, num_actions, hidden_units, gamma, max_experiences, min_experiences, batch_size, lr)
-    N = 50000 
+    N = 50000
     total_rewards = np.empty(N)
-    epsilon = 0.99 
-    decay = 0.9999 
-    min_epsilon = 0.1 
+    epsilon = 0.99
+    decay = 0.9999
+    min_epsilon = 0.1
     for n in range(N):
-        epsilon = max(min_epsilon, epsilon * decay) 
+        epsilon = max(min_epsilon, epsilon * decay)
         total_reward, losses = play_game(env, TrainNet, TargetNet, epsilon, copy_step)
         total_rewards[n] = total_reward
         avg_rewards = total_rewards[max(0, n - 100):(n + 1)].mean()
@@ -144,7 +145,7 @@ def main():
             print("episode:", n, "episode reward:", total_reward, "eps:", epsilon, "avg reward (last 100):", avg_rewards,
                   "episode loss: ", losses)
     print("avg reward for last 100 episodes:", avg_rewards)
-    #env.close()
+    env.close()
 
 
 if __name__ == '__main__':
